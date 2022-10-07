4c75a6e189b9ac48c0eb557dd013f6e100193590
FelipheFaleiros <feliphefaleiros@hotmail.com>
2022-10-07 14:01:48 -0300

    projeto mais adiantado

---
+++ 2/Orenda/Orenda/Controllers/ProdutoController.cs
@@ -0,0 +1,25 @@
+﻿using Orenda.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Orenda.Controllers
+{
+    public class ProdutoController : Controller
+    {
+        // GET: Produto
+        public ActionResult Cadastro()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Cadastrar(Produtos cadastrar)
+        {
+            cadastrar.Cadastrar();
+            return Content("TOP");
+        }
+    }
+}
